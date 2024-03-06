1edaded2af970e793a0294672cf33f6a07084563
Franco Ortiz <ortizfranco48@yahoo.com>
2024-03-06 10:42:52 -0300

    testing

--- Desafio/testing.js
+++ Desafio/testing.js
@@ -7,7 +7,7 @@ console.log(producto.getProductById(1))
 
 
 const imgM = "https://elegifruta.com.ar/wp-content/uploads/2017/07/manzana_roja.jpg"
-const imgP="https://test.jpg.com"
+const imgP="https://images3.memedroid.com/images/UPLOADED383/6298c09c12f1d.jpeg"
 console.log(producto.addProduct('manzana','roja',300, imgM, 'sw71', 200))
 console.log(producto.addProduct('pera','verde',100, imgP, 'sw71', 200))
 console.log(producto.addProduct('pera','verde',100, imgP, 'sw72', 200))
