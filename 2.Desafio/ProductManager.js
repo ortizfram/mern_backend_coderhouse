8a648f241c182734d01656a32880e09b804255ef
Franco Ortiz <ortizfranco48@yahoo.com>
2024-03-19 19:23:10 -0300

    desafio slice products

---
+++ 2.Desafio/ProductManager.js
@@ -0,0 +1,105 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+class ProductManager {
+  constructor() {
+    this.products = [];
+    this.path = "Products.json";
+  }
+
+  getData = async () => {
+    //   retorna readFile data
+    const data = await fs.promises.readFile(this.path);
+    return JSON.parse(data);
+  };
+
+  async addProduct(title, description, price, thumbnail, stock) {
+    try {
+      let products = this.products;
+
+      // Generar el nuevo producto
+      const id = products.length + 1;
+      const code = crypto.randomBytes(4).toString("hex");
+      const product = {
+        id,
+        code,
+        title,
+        description,
+        price,
+        thumbnail,
+        stock,
+      };
+
+      // Agregar el nuevo producto a la lista
+      products.push(product);
+
+      // Escribir los datos actualizados en el archivo
+      await fs.promises.writeFile(this.path, JSON.stringify(products));
+
+      console.log("New products added!"); // Devolver el nuevo producto añadido
+    } catch (error) {
+      throw new Error("Error adding product: " + error.message);
+    }
+  }
+  getProducts = async (limit) => {
+    try {
+      let products = await this.getData();
+      products = products.slice(0, limit)
+
+      return products;
+    } catch (error) {
+      console.error("Error reading file" + error.message);
+    }
+  };
+  getProductById = async (id) => {
+    try {
+      const products = await this.getData();
+      const product = products.find((product) => product.id === id);
+      if (!product) {
+        throw new Error("Product not found");
+      }
+      return product;
+    } catch (error) {
+      throw new Error("Error fetching product: " + error.message);
+    }
+  };
+  updateProduct = async (id, title, description, price, thumbnail, stock) => {
+    const products = await this.getData();
+    const productIndex = products.findIndex((product) => product.id === id);
+
+    if (productIndex === -1) {
+      throw new Error("Product not found");
+    }
+
+    products[productIndex] = {
+      id,
+      title,
+      description,
+      price,
+      thumbnail,
+      stock,
+    };
+
+    fs.writeFileSync(this.path, JSON.stringify(products, null, 2), "utf-8");
+
+    return products[productIndex];
+  };
+  deleteProduct = async (id) => {
+    const products = await this.getData();
+    const product = products.find(
+      (product) => product.id === id,
+      (err) => {
+        if (err) throw new Error("Product not found");
+      }
+    );
+    // filter out id from array
+    const filteredP = products.filter((product) => product.id !== id);
+
+    // save
+    fs.writeFileSync(this.path, JSON.stringify(filteredP, null, 2));
+
+    return product;
+  };
+}
+
+module.exports = ProductManager;
--- 2.Desafio/app.js
+++ 2.Desafio/app.js
@@ -1,11 +1,28 @@
-const express = require("express")
+const express = require("express");
+const path = require("./Products.json");
+const ProductManager = require("./ProductManager");
 
-const app = express()
+const app = express();
+app.use(express.urlencoded({extended:true}))
 
-app.get("/",(req,res)=>{
-    res.send('home');
-})
+const manager = new ProductManager();
 
-app.listen(8080, ()=> {
-    console.log("listening to http://localhost:8080")
-})
+// routes
+app.get("/", (req, res) => {
+  res.send("<button><a href='/products?limit=2&page=1'>Get products</a></button>");
+});
+
+app.get("/products", async (req, res) => {
+  let products = [];
+  let {limit} = req.query ? req.query : 2
+  try {
+    products = await manager.getProducts(limit);
+  } catch (error) {
+    console.error(error);
+  }
+  return res.status(200).json(products);
+});
+
+app.listen(8080, () => {
+  console.log("listening to http://localhost:8080");
+});
