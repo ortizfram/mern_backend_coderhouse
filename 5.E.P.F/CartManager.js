cea87ad00f2d5a3b3fce63b4913e95df51831ca7
Franco Ortiz <ortizfranco48@yahoo.com>
2024-03-27 13:07:23 -0300

    cart class setup

---
+++ 5.E.P.F/CartManager.js
@@ -0,0 +1,19 @@
+class CartManager {
+    constructor() {
+
+    }
+
+    async newCart(){
+
+    }
+
+    async listProdsInCart(cid){
+
+    }
+
+    async addProdToCart (){
+
+    }
+}
+
+module.exports = CartManager;
