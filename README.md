f80ff5fa506fd0f11df6ba8e2b02dbb3b30f748d
Franco Ortiz <ortizfranco48@yahoo.com>
2024-04-09 10:31:54 -0300

    hablebars setup

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 ## 10.websockets
 - http vs websockets// axios
 - configurar servers web sockets dentro de server express
-
 - ingresar web sockets en plantillas
 
--- src/app.js
+++ src/app.js
@@ -3,10 +3,17 @@ const handlebars = require("express-handlebars");
 
 const app = express();
 
+// views/layouts/main.hablebars
+// views/index.hablebars
+
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req,res)=>{
+    res.render("index")
+})
 
 app.listen(8080, () => {
   console.log("listening to 8080");
