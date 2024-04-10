508c843ef8b7d9b508d128b0302a3596f6881317
Franco Ortiz <ortizfranco48@yahoo.com>
2024-04-10 12:37:23 -0300

    websockets setup

--- D.E.1/src/app.js
+++ D.E.1/src/app.js
@@ -3,8 +3,16 @@ const http = require("http");
 const socketIo = require("socket.io");
 const indexRoutes = require("./routes/index.router.js");
 const handlebars = require("express-handlebars");
-
+const ProductRoute = require("./routes/product.routes.js");
+const CartRoute = require("./routes/cart.routes.js");
+const HomeRoute = require("./routes/home.routes.js");
+const cors = require("cors")
+// -----------------------------------
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
 const httpServer = http.createServer(app);
 const io = socketIo(httpServer);
 
@@ -21,26 +29,30 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use("/", indexRoutes);
+app.use("/", HomeRoute);
+app.use("/api/product", ProductRoute);
+app.use("/api/cart", CartRoute);
 
-let messages = [];
 
+let products = [];
+
+// socket handshake
 io.on("connection", (socket) => {
   console.log("Nuevo cliente conectado");
 
   // Enviamos los mensajes almacenados al cliente recién conectado
-  socket.emit("initialMessages", messages);
+  socket.emit("initialProducts", products);
 
-  socket.on("message", (data) => {
-    console.log(data);
-    const newMessage = {
-      socketid: socket.id,
-      message: data
-    };
-    messages.push(newMessage);
-    // Emitimos el nuevo mensaje a todos los clientes
-    io.emit("message", newMessage);
-  });
+  // socket.on("message", (data) => {
+  //   console.log(data);
+  //   const newMessage = {
+  //     socketid: socket.id,
+  //     message: data
+  //   };
+  //   messages.push(newMessage);
+  //   // Emitimos el nuevo mensaje a todos los clientes
+  //   io.emit("message", newMessage);
+  // });
 });
 
 httpServer.listen(8080, () => {
---
+++ D.E.1/src/routes/cart.routes.js
@@ -0,0 +1,59 @@
+const { Router } = require("express");
+const { uploader } = require("../utils/multer");
+const CartManager = require("../CartManager.js");
+
+const cartManager = new CartManager();
+const router = Router();
+
+// crear carrito
+router.post("/", async (req, res) => {
+  try {
+    const nuevoCarrito = await cartManager.newCart();
+    return res
+      .status(201)
+      .json({ success: true, message: "newCart", nuevoCarrito });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error creado carrito", error: error.message });
+  }
+});
+
+// listar productos del carrito
+router.get("/:cid", async (req, res) => {
+  const cid = parseInt(req.params.cid);
+  try {
+    const carrito = await cartManager.listProdsInCart(cid);
+    if (carrito) {
+      return res
+        .status(200)
+        .json({ success: "listCart", carrito });
+    } else {
+      return res.status(404).json({ message: "Cart not found", carrito: [] });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+});
+
+// Agregar Prod. a carrito
+router.post("/:cid/product/:pid", async (req, res) => {
+  try {
+    const agregado = await cartManager.addProdToCart(
+      parseInt(req.params.cid),
+      parseInt(req.params.pid)
+    );
+    return res
+      .status(201)
+      .json({ success: true, message: "addedToCart", agregado });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error agregando Prod a Carrito",
+      error: error.message,
+    });
+  }
+});
+
+module.exports = router;
