282b113c5722171a9c1daa11d4ff6bd3b49370f5
Franco Ortiz <ortizfranco48@yahoo.com>
2024-05-17 18:58:07 -0300

    just github Oauth is left

--- acc1/src/controllers/products/products.controller.js
+++ acc1/src/controllers/products/products.controller.js
@@ -2,15 +2,19 @@ const ProductManager = require("../../ProductManager.js");
 const pm = new ProductManager();
 
 const getProducts = async (req, res) => {
-  const user = req.user;
-
-  try {
-    const products = await pm.getProducts();
-    res.render("products", { user: user, products: products, admin: user && user.role === "admin" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+    const user = req.user;
+  
+    try {
+      const products = await pm.getProducts();
+      const firstName = user ? user.first_name : null;  // Extract first_name or set to null if user is not available
+      const isAdmin = user && user.role === "admin";  // Determine if the user is an admin
+      res.render("products", { firstName: firstName, products: products, admin: isAdmin });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+  
 
 module.exports = {
   getProducts,
