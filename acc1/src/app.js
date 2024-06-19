28dcf74e1d0e88f7fed64bb5759389b08bfb6002
Franco Ortiz <ortizfranco48@yahoo.com>
2024-06-19 13:52:03 -0300

    twilio

--- acc1/src/app.js
+++ acc1/src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth/auth.routes.js");
 const productsRouter = require("./routes/products/products.routes.js");
+const mailingRouter = require("./routes/mailing/mailing.routes.js");
+const smsRouter = require("./routes/sms/sms.routes.js");
 const handlebars = require("express-handlebars");
 const session = require("express-session");
 const mongoose = require("mongoose");
@@ -9,26 +11,29 @@ const passport = require("passport");
 const initializePassport = require("./config/initializePassport.js");
 const router = require("./routes/index/index.routes.js");
 const { MONGODB_URI, SESSION_SECRET } = require("./config/config.js");
-
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 // Initialize Passport
 initializePassport();
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Failed to connect to MongoDB', err));
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
 
 app.use(express.static(__dirname + "/public"));
 app.engine("handlebars", handlebars.engine());
@@ -39,7 +44,10 @@ app.use(cookieParser(SESSION_SECRET)); // pass secret from .env
 app.use("/", router);
 app.use("/api/sessions", authRouter);
 app.use("/api/products", productsRouter);
+app.use("/api/mail", mailingRouter);
+app.use("/api/sms", smsRouter);
 
 app.listen(8080, () => {
   console.log("listening to port 8080");
 });
+
