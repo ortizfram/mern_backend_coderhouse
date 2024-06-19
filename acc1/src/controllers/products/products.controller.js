95f80d09934a9f4ebdd68e36f58f96ec83075d2f
Franco Ortiz <ortizfranco48@yahoo.com>
2024-06-19 20:06:34 -0300

    prod view

--- acc1/src/controllers/products/products.controller.js
+++ acc1/src/controllers/products/products.controller.js
@@ -8,10 +8,8 @@ const getProducts = async (req, res) => {
       const products = await pm.getProducts();
       const firstName = user ? user.first_name : null;  // Extract first_name or set to null if user is not available
       const isAdmin = user && user.role === "admin";  // Determine if the user is an admin
-      console.log(" ")
-      console.log(products)
-      console.log(" ")
-      res.render("products", { firstName: firstName, products: products, admin: isAdmin });
+      console.log("admin",isAdmin)
+      res.render("products", { firstName: firstName,products: products, isAdmin: isAdmin });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
