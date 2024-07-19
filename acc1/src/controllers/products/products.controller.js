d5980770fbd2e9624693e0a92053ab713cd151de
Franco Ortiz <ortizfranco48@yahoo.com>
2024-07-19 13:48:39 -0300

    isPremium products validation

--- acc1/src/controllers/products/products.controller.js
+++ acc1/src/controllers/products/products.controller.js
@@ -8,7 +8,8 @@ const getProducts = async (req, res) => {
       const products = await pm.getProducts();
       const firstName = user ? user.first_name : null;  // Extract first_name or set to null if user is not available
       const isAdmin = user && user.role === "admin";  // Determine if the user is an admin
-      res.render("products", { firstName: firstName, products: products, admin: isAdmin });
+      const isPremium = user && user.role === "premium"
+      res.render("products", { firstName: firstName, products: products, admin: isAdmin, premium:isPremium });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
