0434f7d6df7abe4c5dd4290c61d9232e0616ca5b
Franco Ortiz <ortizfranco48@yahoo.com>
2024-07-22 11:09:32 -0300

    clone

---
+++ acc1/src/app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRouter = require("./routes/auth/auth.routes.js");
+const productsRouter = require("./routes/products/products.routes.js");
+const mailingRouter = require("./routes/mailing/mailing.routes.js");
+const smsRouter = require("./routes/sms/sms.routes.js");
+const cartRouter = require("./routes/cart/cart.routes.js");
+const handlebars = require("express-handlebars");
+const { create } = require('express-handlebars');
+const session = require("express-session");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const initializePassport = require("./config/initializePassport.js");
+const router = require("./routes/index/index.routes.js");
+const { MONGODB_URI, SESSION_SECRET } = require("./config/config.js");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+// Initialize Passport
+initializePassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+app.use(express.static(__dirname + "/public"));
+const hbs = create({
+  // Specify runtime options
+  defaultLayout: 'main',
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true,
+  }
+});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+
+app.use(cookieParser(SESSION_SECRET)); // pass secret from .env
+app.use("/", router);
+app.use("/api/sessions", authRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/mail", mailingRouter);
+app.use("/api/sms", smsRouter);
+
+// http y socket
+const { Server } = require("socket.io");
+const serverHTTP = app.listen(8080, () => console.log(`http & express on port 8080`));
+const io = new Server(serverHTTP);
+
+io.on("connection", (socket) => {
+  console.log("socket connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("socket disconnected");
+  });
+});
+
+// app.listen(8080, () => {
+//   console.log("express on port 8080");
+// });
---
+++ acc1/src/controllers/auth/auth.controller.js
@@ -0,0 +1,122 @@
+const passport = require("passport");
+const User = require("../../dao/models/user.model");
+const { createHash, isValidPassword } = require("../../utils/utils");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config/config");
+
+const generateJWT = (user) => {
+  const payload = { id: user._id, email: user.email, role: user.role };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return token;
+};
+
+const home = (req, res) => {
+  let user = req.session.user;
+  res.render("login", { user: user });
+};
+
+// Middleware to protect routes
+const middlewareAuth = (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).send("Authentication error");
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
+const logoutConSession = (req, res) => {
+  res.clearCookie("jwt"); // Clear the JWT cookie
+  res.redirect("/api/sessions/login"); // Redirect to login page after logout
+};
+
+const getRegister = (req, res) => {
+  res.render("registro", {});
+};
+
+const getLogin = (req, res) => {
+  res.render("login", {});
+};
+
+const perfil = (req, res) => {
+  const user = req.user;
+  res.render("perfil", { user: user });
+};
+
+const getResetPassword = (req, res) => {
+  res.render("resetPassword", {});
+};
+
+const postResetPassword = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    const hashedPassword = createHash(password);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.status(200).json({ message: "Password reset successfully" });
+  } catch (error) {
+    console.error("Reset error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const loginUser = (req, res) => {
+  passport.authenticate("login", (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    req.login(user, { session: false }, (err) => {
+      if (err) {
+        return res.status(500).json({ message: "Internal server error" });
+      }
+      const token = generateJWT(user);
+      console.log("token: ", token);
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "Strict",
+      }); // Adjust 'secure' according to your environment
+      return res.json({ message: "Logged in successfully" });
+    });
+  })(req, res);
+};
+
+const getCurrentUser = (req, res,next) => {
+  const user = User.findOne({ _id: req.user });
+  if (user) {
+    const { password, ...userWithoutPassword } = req.user.toObject();
+    req.user = userWithoutPassword
+    next()
+  } else {
+    res.status(404).json({ error: "error de passport" });
+  }
+};
+
+module.exports = {
+  loginUser,
+  getCurrentUser,
+  generateJWT,
+  getResetPassword,
+  postResetPassword,
+  perfil,
+  getLogin,
+  getRegister,
+  logoutConSession,
+  home,
+  middlewareAuth,
+};
